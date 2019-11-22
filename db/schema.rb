0b253ebede7b974974605694597ba652f6bf986c
Victoria Hale <vic.hale74@gmail.com>
2019-11-22 17:46:40 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20191121214421) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "passwrod"
+    t.string "password"
     t.float "balance"
   end
 
