65d287cf6a39bd68aa5be9e73dad682340af806a
Victoria Hale <vic.hale74@gmail.com>
2019-11-22 17:39:38 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191121214421) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "passwrod"
-    t.float "balance"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
