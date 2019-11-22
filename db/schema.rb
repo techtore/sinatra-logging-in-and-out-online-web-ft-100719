b6deb332187c318a4d14aaf47d73b98ab11fa6e5
Victoria Hale <vic.hale74@gmail.com>
2019-11-22 17:41:44 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20191121214421) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "passwrod"
     t.float "balance"
   end
 
